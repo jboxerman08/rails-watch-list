569f767a12f4bfff5d9c1b0d17b4b6cc1fd44472
Jeremy Boxerman <jboxerman08@gmail.com>
2021-09-27 20:59:31 -0400

    seeded the movieDB popular movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,11 @@ Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s f
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
-require 'json'
-require 'open-uri'
-
-url = 'https://api.themoviedb.org/3/movie/553?api_key=ENV['IMDB_API_KEY']'
-movie_serialized = URI.open(url).read
-movie = JSON.parse(movie_serialized)
-
-# file = URI.open('https://api.themoviedb.org/3/movie/553?api_key=IMDB_API_KEY')
-# list.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+TheMovieDBService.popular_movies.each do |movie|
+  Movie.create(
+    title: movie[:title],
+    overview: movie[:overview],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie[:poster_path]}",
+    rating: movie[:vote_average]
+  )
+end
